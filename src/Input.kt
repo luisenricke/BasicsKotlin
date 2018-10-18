e3d93c11a4d9afa270b79946e98092b13dd9e233
luisenricke <36013173+luisenricke@users.noreply.github.com>
2018-10-18 00:32:24 -0500

    Initial commit

---
+++ src/Input.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+    /*println("How are u?")
+    var feel = readLine()
+    println("I hope u are well")
+    println("How old are u?")
+    var age: Int? = readLine()!!.toInt()
+    println("U have "+ age)*/
+
+    var num1 : Int = readLine()!!.toInt();
+    var num2 : Int = readLine()!!.toInt();
+    println("the sum is " + (num1+num2) + "\n" +
+            "the substraction is " + (num1-num2) + "\n" +
+            "the multiplication is " + (num1*num2) + "\n" +
+            "the division is " + (num1/num2))
+}
---
+++ src/VariablesCommentsConstant.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+
+    var myPlaceHolder = "My value"
+    //println(myPlaceHolder)
+
+   /*
+   myPlaceHolder = "Another value"
+    println(myPlaceHolder)
+    */
+
+    var age: Int = 22
+    var name: String = "Luis "
+    var lastName: String = "Villalobos"
+    var height: Float = 178.2f
+    println("Hello my name is " + name + lastName)
+    println("I'm " + age + " years old and " + height + " tall")
+
+    val PI: Double = 3.1416
+    println(PI)
+
+    var myVariableNull : String? //Variable optional
+    myVariableNull = null
+    println(myVariableNull)
+
+    /* Error, not null
+    var anotherVarible: String
+    anotherVarible = null
+    */
+
+
+}
