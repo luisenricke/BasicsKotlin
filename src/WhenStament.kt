c10eeacde381e84443b807e62742e15bd5611c67
luisenricke <36013173+luisenricke@users.noreply.github.com>
2018-10-22 21:17:56 -0500

    Learning When Statements

---
+++ src/WhenStament.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+
+    var personAge = 22
+
+    when (personAge) { //It's like a switch
+        1 -> {
+            println("It's a baby with $personAge")
+        }
+
+        5,6,7,8,9,10 -> {
+            println("It's a boy with $personAge")
+        }
+
+        in 11..19->{
+            println("Its a teen with $personAge")
+        }
+        /*
+        !in 2..4 ->{
+            println("It's not a baby")
+        }*/
+
+        else ->{
+            println("You're probably dead")
+        }
+    }
+}
