f0037bfaa72d4235b4d2bd156f21170c43da4504
luisenricke <36013173+luisenricke@users.noreply.github.com>
2018-10-24 04:46:35 -0500

    Learning create a custom constructors

--- src/SimpleClass.kt
+++ src/SimpleClass.kt
@@ -1,42 +1,54 @@
 fun main(args: Array<String>) {
 
-    var myCar : Car = Car()
-    myCar.speed=100
+    var myCar: Car = Car()
+    myCar.speed = 100
     println(myCar.speed)
 
-    var urCar : Car = Car()
-    urCar.power=300
+    var urCar: Car = Car()
+    urCar.power = 300
     println(urCar.power)
     println()
     println(Car().numberOfWheels)
 
-    var myBoxer: Boxer = Boxer("Luis",100,100)
-
+    var myBoxer: Boxer = Boxer("Luis", 100, 100)
+    println()
+    var Animal: Animal = Animal("Lion", "Yellow", 100, 100)
+    var ohterAnimal: Animal = Animal(300)
 
 }
 
-class Boxer(name:String, power:Int,speed:Int){
-    private var name : String = ""
+class Animal(power: Int) {
+    init {
+        println(power)
+    }
+
+    constructor(name: String, color: String, speed: Int, power: Int) : this(power) {
+        println("$name - $color - $speed - $power")
+    }
+}
+
+class Boxer(name: String, power: Int, speed: Int) {
+    private var name: String = ""
     private var power: Int? = null
-    private var speed : Int = 0
+    private var speed: Int = 0
 
     init {
         this.name = name
         this.speed = speed
-        println(name + " - " + power + " - "+ speed )
+        println(name + " - " + power + " - " + speed)
     }
 
-    fun getName(): String{
+    fun getName(): String {
         return this.name
     }
 
 }
 
-class Car{
+class Car {
     var speed: Int = 0
     var power: Int = 0
     var name: String = ""
-    var numberOfWheels: Int =0
+    var numberOfWheels: Int = 0
 
 
 }
