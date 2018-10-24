3fbab03eb36f1fe7b5c83f91f6e5b9d259ffe562
luisenricke <36013173+luisenricke@users.noreply.github.com>
2018-10-24 08:03:54 -0500

    Learning sinleton Pattern

---
+++ src/PatternDesign/Sigleton.kt
@@ -0,0 +1,22 @@
+package PatternDesign
+
+import sun.security.pkcs11.Secmod
+
+fun main(args: Array<String>) {
+    var db = DBInstance.instance
+    println(db.dbname)
+    db.dbname = "Horarios"
+    println(db.dbname)
+}
+
+class DBInstance{
+    var dbname : String = "Tienda"
+
+    private constructor(){
+        println("database created")
+    }
+
+    companion object {
+        val instance: DBInstance by lazy { DBInstance() }
+    }
+}
