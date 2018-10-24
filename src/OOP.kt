9bd5c5198ae6dd155163cf04e955a1b89ad4b09f
luisenricke <36013173+luisenricke@users.noreply.github.com>
2018-10-24 05:22:33 -0500

    Learning Overriding functions

---
+++ src/OOP.kt
@@ -0,0 +1,38 @@
+fun main(args: Array<String>) {
+
+    var myPerson:Person = Person()
+    myPerson.name="Luis"
+    println(myPerson.name)
+
+    var myStudent:Student = Student()
+    myStudent.name="Luis"
+    myStudent.StudentID=123
+    println(myStudent.StudentID!!)
+
+    var overridePerson : Person = Person()
+    overridePerson.older()
+
+    var overrideStudent : Student = Student()
+    overrideStudent.older()
+}
+
+open class Person() {
+    var name: String? = null
+    var age: Int? = null
+    var height: Int? = null
+
+     open fun older(){
+        println("The person is alive")
+    }
+}
+
+class Student(): Person(){
+    var StudentID: Int?=null
+     override fun older(){
+         println("The person is a student")
+     }
+}
+
+class Employee ():Person(){
+    var EmployeeID:Int?=null
+}
