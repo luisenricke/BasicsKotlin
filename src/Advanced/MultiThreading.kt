b94029537a22b2c9f811ce4b9e39837b9257d877
luisenricke <36013173+luisenricke@users.noreply.github.com>
2018-12-03 00:28:47 -0600

    Learning Multi-threading

--- src/Advanced/MultiThreading.kt
+++ src/Advanced/MultiThreading.kt
@@ -1,5 +1,43 @@
 package Advanced
 
-fun main(args: Array<String>) {
+import java.lang.Exception
 
+fun main(args: Array<String>) {
+    var lion = Animal("Lion")
+    lion.start()
+
+    lion.join()
+
+    var elephant = Animal("Elephant")
+    elephant.start()
+
+    var monkey = Animal("Monkey")
+    monkey.start()
+
+    var bear = Animal("Bear")
+    bear.start()
+}
+
+class Animal: Thread{
+    var animalName: String = ""
+
+    constructor(name:String){
+        animalName = name
+    }
+
+    override fun run() {
+        super.run()
+        var counter = 0
+
+        while (counter <5){
+            println("$animalName is running - $counter")
+            counter++
+
+            try {
+                Thread.sleep(2000)
+            }catch (ex:Exception){
+                println(ex.message)
+            }
+        }
+    }
 }
